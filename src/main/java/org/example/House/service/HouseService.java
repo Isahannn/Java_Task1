eef0f12373360aaa510b155271af5970efa77bad
Isahan <lolwindy@mail.ru>
2025-03-26 14:08:32 +0300

    commit 5

--- src/main/java/org/example/House/service/HouseService.java
+++ src/main/java/org/example/House/service/HouseService.java
@@ -8,13 +8,10 @@ public interface HouseService {
     void addHouse(House house);
     void removeHouse(House house);
     List<House> findAllHouses();
-
-    // Старые методы
     List<House> findHousesWithRooms(int rooms);
     List<House> findHousesWithRoomsAndFloorRange(int rooms, int minFloor, int maxFloor);
     List<House> findHousesWithAreaGreaterThan(double area);
 
-    // Новые методы с Optional
     Optional<House> findHouseById(int id);
     Optional<List<House>> findHousesWithRoomsOptional(int rooms);
     Optional<List<House>> findHousesWithAreaGreaterThanOptional(double area);
